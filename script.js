d691a241a216d62adedbc5c782e25bbbf80e67b5
maccha <chihokoku8060@gmail.com>
2024-11-28 17:30:35 +0900

    add:feature of coordinate transformation(translation and rotate)

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function init() {
     canvas: canvas1,
   });
   const width = 800;
-  const height = 500;
+  const height = 600;
   renderer1.setSize(width, height);
 
   // カメラ1の設定
   const fov = 75;
   const aspect = 2;
   const near = 0.1;
-  const far = 1000;
+  const far = 10000;
   const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera1.position.set(0, -200, 0);
 
@@ -46,11 +46,10 @@ function init() {
   const canvas2 = document.getElementById("canvas2");
   const ctx2 = canvas2.getContext("2d");
 
-  let object1;
-  // ファイル入力要素の取得
-  const fileInput = document.getElementById("fileInput");
-  // ファイルが選択されたときの処理
-  fileInput.addEventListener("change", function (event) {
+  // 大腿骨ファイル入力要素の取得
+  let femur;
+  const femurfileInput = document.getElementById("femurfileInput");
+  femurfileInput.addEventListener("change", function (event) {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -58,9 +57,28 @@ function init() {
         const contents = e.target.result;
         // OBJLoaderで読み込み
         const objLoader = new THREE.OBJLoader();
-        object1 = objLoader.parse(contents);
-        object1.position.set(0, 0, 0);
-        scene1.add(object1);
+        femur = objLoader.parse(contents);
+        femur.position.set(0, 0, 0);
+        scene1.add(femur);
+      };
+      reader.readAsText(file);
+    }
+  });
+
+  // 脛骨ファイル入力要素の取得
+  let tibia;
+  const tibiafileInput = document.getElementById("tibiafileInput");
+  tibiafileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const contents = e.target.result;
+        // OBJLoaderで読み込み
+        const objLoader = new THREE.OBJLoader();
+        tibia = objLoader.parse(contents);
+        tibia.position.set(0, 0, 0);
+        scene1.add(tibia);
       };
       reader.readAsText(file);
     }
@@ -92,31 +110,116 @@ function init() {
     }
   });
 
-  // 「Right」ボタンを押したら3Dモデルを右に移動
-  const rightButton = document.getElementById("rightButton");
-  rightButton.addEventListener("click", function () {
-    if (object1) {
-      object1.position.x += 5; // 右方向（+X軸）に10単位移動
-    }
+  // *********************************************************************
+  //                 大腿骨の設定座標を変えるプログラム
+  // *********************************************************************
+  // 下記で並進を変更
+  const femurTranslation = document.getElementById("femurTranslation");
+  femurTranslation.addEventListener("click", function () {
+    let femurTranslationX = parseFloat(
+      document.getElementById("femurTranslationX").value
+    );
+    let femurTranslationY = parseFloat(
+      document.getElementById("femurTranslationY").value
+    );
+    let femurTranslationZ = parseFloat(
+      document.getElementById("femurTranslationZ").value
+    );
+    // それぞれの座標値を変更
+    femur.position.x = femurTranslationX;
+    femur.position.y = femurTranslationY;
+    femur.position.z = femurTranslationZ;
   });
+
+  // 下記で回転を変更
+  const femurRotate = document.getElementById("femurRotate");
+  femurRotate.addEventListener("click", function () {
+    let femurRotateX = parseFloat(
+      document.getElementById("femurRotateX").value
+    );
+    let femurRotateY = parseFloat(
+      document.getElementById("femurRotateY").value
+    );
+    let femurRotateZ = parseFloat(
+      document.getElementById("femurRotateZ").value
+    );
+    // ラジアンに変換
+    const RadiansFemurRotateX = (femurRotateX * Math.PI) / 180;
+    const RadiansFemurRotateY = (femurRotateY * Math.PI) / 180;
+    const RadiansFemurRotateZ = (femurRotateZ * Math.PI) / 180;
+    // それぞれの座標値を変更
+    femur.rotation.x = RadiansFemurRotateX;
+    femur.rotation.y = RadiansFemurRotateY;
+    femur.rotation.z = RadiansFemurRotateZ;
+  });
+
+  // *********************************************************************
+  //                 脛骨の設定座標を変えるプログラム
+  // *********************************************************************
+  // 下記で並進を変更
+  const tibiaTranslation = document.getElementById("tibiaTranslation");
+  tibiaTranslation.addEventListener("click", function () {
+    let tibiaTranslationX = parseFloat(
+      document.getElementById("tibiaTranslationX").value
+    );
+    let tibiaTranslationY = parseFloat(
+      document.getElementById("tibiaTranslationY").value
+    );
+    let tibiaTranslationZ = parseFloat(
+      document.getElementById("tibiaTranslationZ").value
+    );
+    tibia.position.x = tibiaTranslationX;
+    tibia.position.y = tibiaTranslationY;
+    tibia.position.z = tibiaTranslationZ;
+  });
+
+  // 下記で回転を変更
+  const tibiaRotate = document.getElementById("tibiaRotate");
+  tibiaRotate.addEventListener("click", function () {
+    let tibiaRotateX = parseFloat(
+      document.getElementById("tibiaRotateX").value
+    );
+    let tibiaRotateY = parseFloat(
+      document.getElementById("tibiaRotateY").value
+    );
+    let tibiaRotateZ = parseFloat(
+      document.getElementById("tibiaRotateZ").value
+    );
+    // ラジアンに変換
+    const RadiansTibiaRotateX = (tibiaRotateX * Math.PI) / 180;
+    const RadiansTibiaRotateY = (tibiaRotateY * Math.PI) / 180;
+    const RadiansTibiaRotateZ = (tibiaRotateZ * Math.PI) / 180;
+    // それぞれの座標値を変更
+    tibia.rotation.x = RadiansTibiaRotateX;
+    tibia.rotation.y = RadiansTibiaRotateY;
+    tibia.rotation.z = RadiansTibiaRotateZ;
+  });
+
+  // 「Right」ボタンを押したら3Dモデルを右に移動
+  // const rightButton = document.getElementById("rightButton");
+  // rightButton.addEventListener("click", function () {
+  //   if (object1) {
+  //     object1.position.x += 5; // 右方向（+X軸）に10単位移動
+  //   }
+  // });
 
   // 「Left」ボタンを押したら3Dモデルを右に移動
-  const leftButton = document.getElementById("leftButton");
-  leftButton.addEventListener("click", function () {
-    if (object1) {
-      object1.position.x -= 5;
-    }
-  });
+  // const leftButton = document.getElementById("leftButton");
+  // leftButton.addEventListener("click", function () {
+  //   if (object1) {
+  //     object1.position.x -= 5;
+  //   }
+  // });
 
   // 回転ボタン要素を取得
-  const rotateButton = document.getElementById("rotateButton");
-  rotateButton.addEventListener("click", () => {
-    if (object1) {
-      // 回転を適用（y軸周りに回転させる例）
-      const rotationAngle = Math.PI / 16; // 1回転で45°回転
-      object1.rotation.z += rotationAngle;
-    }
-  });
+  // const rotateButton = document.getElementById("rotateButton");
+  // rotateButton.addEventListener("click", () => {
+  //   if (object1) {
+  //     // 回転を適用（y軸周りに回転させる例）
+  //     const rotationAngle = Math.PI / 16; // 1回転で45°回転
+  //     object1.rotation.z += rotationAngle;
+  //   }
+  // });
 
   // 輪郭を抽出して 2D に描画する関数
   const outLine = document.getElementById("outLine");
